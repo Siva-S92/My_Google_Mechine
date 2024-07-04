e87e70d9375aeb756c95d96ca2491d057cc6bd00
Siva-S92 <sivagraphics4fashion@gmail.com>
2024-07-04 16:07:49 +0530

    handle possible errors from server side

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
-function WebSearchpage() {
+async function WebSearchpage({ searchParams }) {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  );
+  if (!response.ok) throw new Error("Something went wrong");
+  const data = await response.json();
+  const results = data.items;
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">
+          No results found for {searchParams.searchTerm}
+        </h1>
+        <p className="text-lg">
+          Try searching with something else, or{" "}
+          <Link href="/" className="text-blue-500 underline italic">
+            go to home
+          </Link>
+        </p>
+      </div>
+    );
+  }
   return (
-    <div className='text-red-500'>WebSearchpage</div>
-  )
+    <div>{results && results.map((result) => <h1>{result.title}</h1>)}</div>
+  );
 }
 
-export default WebSearchpage
+export default WebSearchpage;
